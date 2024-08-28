15c867926959b6f38e47dd864a2b89e7f11b9a0c
fjsanhueza1994 <43219924+fjsanhueza1994@users.noreply.github.com>
2024-08-28 19:58:49 +0000

    Challenge 2,3: Adding new funcionality and make first Pull Request

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,26 @@
 // Placeholder to host a component
+import React from "react";
+
+const Hours = () => {
+    const shelterHours = {
+        Monday: "10:00 - 16:00",
+        Tuesday: "10:00 - 16:00",
+        Wednesday: "10:00 - 16:00",
+        Thursday: "10:00 - 16:00",
+        Friday: "10:00 - 16:00",
+        Saturday: "9:00 - 20:00",
+        Sunday: "9:00 - 20:00",
+    };
+
+    const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    const todayHours = shelterHours[today] || "Closed";
+
+    return (
+        <div id="hours">
+            <h2>Horario de atención </h2>
+            <p>{today} {todayHours}</p>
+        </div>
+    );
+};
+
+export default Hours;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,55 +3,60 @@ import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
 
 // TODO: Import Hours component
+import Hours from "../components/Hours";
 
 const Index = ({ pets }) => {
 
   return (
+    
     <>
+      <div>
+        {/* TODO: Display Hours component */}
 
-      {/* TODO: Display Hours component */}
+        {/* Create a card for each pet */}
+        {pets.map((pet) => (
+          <div key={pet._id}>
+            <div className="card">
+              <img src={pet.image_url} />
+              <h5 className="pet-name">{pet.name}</h5>
+              <div className="main-content">
+                <p className="pet-name">{pet.name}</p>
+                <p className="owner">Owner: {pet.owner_name}</p>
 
-      {/* Create a card for each pet */}
-      {pets.map((pet) => (
-        <div key={pet._id}>
-          <div className="card">
-            <img src={pet.image_url} />
-            <h5 className="pet-name">{pet.name}</h5>
-            <div className="main-content">
-              <p className="pet-name">{pet.name}</p>
-              <p className="owner">Owner: {pet.owner_name}</p>
+                {/* Extra Pet Info: Likes and Dislikes */}
+                <div className="likes info">
+                  <p className="label">Likes</p>
+                  <ul>
+                    {pet.likes.map((data, index) => (
+                      <li key={index}>{data} </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="dislikes info">
+                  <p className="label">Dislikes</p>
+                  <ul>
+                    {pet.dislikes.map((data, index) => (
+                      <li key={index}>{data} </li>
+                    ))}
+                  </ul>
+                </div>
 
-              {/* Extra Pet Info: Likes and Dislikes */}
-              <div className="likes info">
-                <p className="label">Likes</p>
-                <ul>
-                  {pet.likes.map((data, index) => (
-                    <li key={index}>{data} </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="dislikes info">
-                <p className="label">Dislikes</p>
-                <ul>
-                  {pet.dislikes.map((data, index) => (
-                    <li key={index}>{data} </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className="btn-container">
-                <Link href="/[id]/edit" as={`/${pet._id}/edit`}>
-                  <button className="btn edit">Edit</button>
-                </Link>
-                <Link href="/[id]" as={`/${pet._id}`}>
-                  <button className="btn view">View</button>
-                </Link>
+                <div className="btn-container">
+                  <Link href="/[id]/edit" as={`/${pet._id}/edit`}>
+                    <button className="btn edit">Edit</button>
+                  </Link>
+                  <Link href="/[id]" as={`/${pet._id}`}>
+                    <button className="btn view">View</button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+        <Hours />
+      </div>
     </>
+    
   );
 };
 
